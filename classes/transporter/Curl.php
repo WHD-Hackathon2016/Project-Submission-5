0c9fe99bb3728f1b1b635cf270f70dff9930bd7e
Benjamin Trenkle <benjamin.trenkle@wicked-chick.de>
2016-03-13 15:32:49 +0100

    Add post parameters

--- classes/transporter/Curl.php
+++ classes/transporter/Curl.php
@@ -21,18 +21,43 @@ class Curl extends \transporter\Transporter
 		return $this->request($url, 'GET');
 	}
 
-	public function post($url)
+	public function post($url, $data)
 	{
-		return $this->request($url, 'POST');
+		return $this->request($url, 'POST', $data);
 	}
 
-	public function request($url, $method, $options = array(), $headers = array())
+	public function request($url, $method, $data = null, $options = array(), $headers = array())
 	{
 		$ch = curl_init();
 
 		$options[CURLOPT_CUSTOMREQUEST] = strtoupper($method);
 
 		$options[CURLOPT_URL] = (string) $url;
+
+		if (isset($data))
+		{
+			// If the data is a scalar value simply add it to the cURL post fields.
+			if (is_scalar($data) || (isset($headers['Content-Type']) && strpos($headers['Content-Type'], 'multipart/form-data') === 0))
+			{
+				$options[CURLOPT_POSTFIELDS] = $data;
+			}
+			// Otherwise we need to encode the value first.
+			else
+			{
+				$options[CURLOPT_POSTFIELDS] = http_build_query($data);
+			}
+
+			if (!isset($headers['Content-Type']))
+			{
+				$headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=utf-8';
+			}
+
+			// Add the relevant headers.
+			if (is_scalar($options[CURLOPT_POSTFIELDS]))
+			{
+				$headers['Content-Length'] = strlen($options[CURLOPT_POSTFIELDS]);
+			}
+		}
 
 		$headers['x-token'] = $this->xtoken;
 
